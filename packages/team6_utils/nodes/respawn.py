45dddae8abefc01750651cca7b37f30f4395935a
Jonah Lee <jonahjacqueslee@gmail.com>
2026-03-29 21:26:59 -0700

    Add comment to respawn.py

--- packages/team6_utils/nodes/respawn.py
+++ packages/team6_utils/nodes/respawn.py
@@ -3,5 +3,7 @@
 from move_relative import respawn_model
 
 if __name__ == '__main__':
+    # No need to init_node when gazebo is already runing
+    # it runs a little faster without init_node anyway
     model_name = 'B1'
     respawn_model(model_name)
