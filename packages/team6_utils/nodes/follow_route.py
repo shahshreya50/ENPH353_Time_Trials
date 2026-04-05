3788c721e483ee3b837292423599ed5fdd5995fc
Jonah Lee <jonahjacqueslee@gmail.com>
2026-04-04 23:12:49 -0700

    FIX: Added delay after respawn to prevent missing Wrench messages
    Also fixed subtle mutation bug of car_pos

--- packages/team6_utils/nodes/follow_route.py
+++ packages/team6_utils/nodes/follow_route.py
@@ -47,10 +47,10 @@ def fly_to_carx(x: int,
         drone_pos: np.ndarray = MODEL_POSITIONS['B1_spawnpos']            
 
     car_name = f"car{x}"
-    car_pos: np.ndarray = MODEL_POSITIONS[car_name]
-    
+
     # Fix the car theta so that 0 deg means the text faces +x
-    car_pos += np.array([0, 0, 0, np.pi/2])
+    car_pos: np.ndarray = MODEL_POSITIONS[car_name] + np.array([0, 0, 0, np.pi/2])
+    
 
     # We want to align the rear of the drone with the normal of the sign face
     drone_rear_theta = drone_pos[3] + np.pi
@@ -64,8 +64,8 @@ def fly_to_carx(x: int,
 
     ctrl.increase_position((0, 0, delta_z + vertical_clearance))
     ctrl.increase_position((*(delta_xy + xy_offset), 0))
-    ctrl.increase_position((0, 0, -vertical_clearance + end_height_above))
     ctrl.increase_angle((0, 0, delta_phi))
+    ctrl.increase_position((0, 0, -vertical_clearance + end_height_above))
 
 
 if __name__ == "__main__":
@@ -81,7 +81,7 @@ if __name__ == "__main__":
         MODEL_MASS,
         MODEL_I_ZZ,
         force_offsets=(0, 0, MODEL_MASS * A_GRAVITY),
-        default_impulse_duration=0.5,
+        default_impulse_duration=0.02,
     )
 
     # ==========================================================
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     # node will start a bit after the Gazebo simulation
     respawn_model('B1')
     ctrl.zero_force(with_offset=False)
-    rospy.sleep(1)
+    rospy.sleep(2)
 
     # ==========================================================
     # This is the point from which we assume the node will start
@@ -103,12 +103,13 @@ if __name__ == "__main__":
     ctrl.zero_force(with_offset=True)
     respawn_model('B1')
 
-    for car_i in range(8):
+    while(True):
+        for car_i in range(8):
 
-        rospy.sleep(0.5)
+            rospy.sleep(0.5)
 
-        vertical_clearance = 4 if car_i == 6 else 1
+            vertical_clearance = 4 if car_i == 6 else 1
 
-        ctrl.zero_force(with_offset=True)
-        respawn_model('B1')
-        fly_to_carx(car_i, ctrl, vertical_clearance=vertical_clearance)
+            respawn_model('B1')
+            rospy.sleep(0.5)
+            fly_to_carx(car_i, ctrl, vertical_clearance=vertical_clearance)
